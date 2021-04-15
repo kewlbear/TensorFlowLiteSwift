3acadb5a8e0d3772813f3a951c2aa5bad62ee273
Changbeom Ahn <kewlbear@gmail.com>
2021-04-15 10:19:49 +0900

    fix CI

--- Scripts/pod.sh
+++ Scripts/pod.sh
@@ -1,12 +1,16 @@
 V=$1
 
+git fetch --tags
+
 cd Example
 
 if [ "$V" = "" ]
 then
   sed s/VERSION//g Podfile.in > Podfile
 else
-  sed s/VERSION/", '~> $V'"/g Podfile.in > Podfile
+  (git tag | grep "$V") && exit
+  N=`echo $V | sed 's/\(\.[^0]\)/.1-nightly\1/'`
+  sed s/VERSION/", '~> $N'"/g Podfile.in > Podfile
 fi
 
 pod install
@@ -14,6 +18,7 @@ pod install
 if [ "$V" = "" ]
 then
   V=`egrep -o '(\d+\.)+\d+' Podfile.lock | head -1`
+  (git tag | grep "$V") && exit
 fi
 
 echo "::set-output name=version::$V"
